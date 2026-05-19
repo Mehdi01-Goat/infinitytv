ec4b9d3fb21c15f04b7d04a417e20ea43850ac24
Mehdi01-Goat <sadikmehdi394@gmail.com>
2026-05-19 11:50:12 +0100

    Redirect root / to /infinitytv-iptv (301)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,5 @@
-import AnnouncementBar from "@/components/AnnouncementBar";
-import Navbar from "@/components/Navbar";
-import HeroSection from "@/components/HeroSection";
-import FeaturesGrid from "@/components/FeaturesGrid";
-import TrendingSection from "@/components/TrendingSection";
-import PricingSection from "@/components/PricingSection";
-import DevicesSection from "@/components/DevicesSection";
-import SetupSteps from "@/components/SetupSteps";
-import ComparisonSection from "@/components/ComparisonSection";
-import ReviewsSection from "@/components/ReviewsSection";
-import FaqSection from "@/components/FaqSection";
-import ContactSection from "@/components/ContactSection";
-import CtaBanner from "@/components/CtaBanner";
-import Footer from "@/components/Footer";
-import WhatsAppFloat from "@/components/WhatsAppFloat";
+import { permanentRedirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <div className="min-h-screen">
-      <AnnouncementBar />
-      <Navbar />
-      <HeroSection />
-      <FeaturesGrid />
-      <TrendingSection />
-      <PricingSection />
-      <DevicesSection />
-      <SetupSteps />
-      <ComparisonSection />
-      <ReviewsSection />
-      <FaqSection />
-      <ContactSection />
-      <CtaBanner />
-      <Footer />
-      <WhatsAppFloat />
-    </div>
-  );
+export default function Root() {
+  permanentRedirect("/infinitytv-iptv");
 }
