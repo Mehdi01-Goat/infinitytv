914cda69ceaa551194b982ce219e496927efc740
Mehdi01-Goat <sadikmehdi394@gmail.com>
2026-05-21 11:34:31 +0100

    Add LanguageProvider to Providers

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,13 +3,16 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
 import { useState } from "react";
+import { LanguageProvider } from "@/context/LanguageContext";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <Toaster richColors />
-    </QueryClientProvider>
+    <LanguageProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <Toaster richColors />
+      </QueryClientProvider>
+    </LanguageProvider>
   );
 }
